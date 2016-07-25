c9707ab511d26aa8b31d6cc9fa3fca20808bc881
James <jamesnguyen23@gmail.com>
2016-07-25 18:29:48 -0400

    updated gemfile, modified user model

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160711174301) do
+ActiveRecord::Schema.define(version: 20160725222901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,12 +31,10 @@ ActiveRecord::Schema.define(version: 20160711174301) do
     t.string   "profile_url"
     t.datetime "created_at",                     null: false
     t.datetime "updated_at",                     null: false
-    t.boolean  "online"
     t.datetime "last_active_at"
     t.integer  "mmr",            default: 1000
     t.boolean  "in_game",        default: false
+    t.index ["uid"], name: "index_users_on_uid", unique: true, using: :btree
   end
-
-  add_index "users", ["uid"], name: "index_users_on_uid", unique: true, using: :btree
 
 end
