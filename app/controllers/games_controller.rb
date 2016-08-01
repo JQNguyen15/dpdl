f7fc18098c786a7d8627f20def6cfc3285d2fb92
James <jamesnguyen23@gmail.com>
2016-08-01 17:06:44 -0400

    fixed some bugs, made some more checks in place

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -46,59 +46,67 @@ class GamesController < ApplicationController
 	end
 
 	def start_game
-		@game = Game.find_by(id: params[:gameid])
-		if @game.players.count == 10
-			
-			@game.make_teams
+		if logged_in
+			@game = Game.find_by(id: params[:gameid])
+			if @game.players.count == 10 && @game.host == current_user.id
+				
+				@game.make_teams
 
-			@game.started = true
-			# give all players a vote
-			@game.players.each do |player|
-				player.has_vote = true
-				player.save
+				@game.started = true
+				# give all players a vote
+				@game.players.each do |player|
+					player.has_vote = true
+					player.save
+				end
+				@game.save            
 			end
-			@game.save            
 		end
 	end
 
 	def vote_radiant
-		current_user.has_vote = false
-		current_user.save
+		if logged_in
+			current_user.has_vote = false
+			current_user.save
 
-		@game = Game.find_by(id: params[:gameid])
-		@game.rad_votes += 1
-		if @game.rad_votes >= 6
-			@game.winner = "radiant"
-			@game.loser = "dire"
+			@game = Game.find_by(id: params[:gameid])
+			@game.rad_votes += 1
+			if @game.rad_votes >= 6
+				@game.winner = "radiant"
+				@game.loser = "dire"
+			
+				@game.calc_stakes
+				@game.get_stakes_for_outcome
+				@game.calc_winner_ratings
+				@game.calc_loser_ratings
+				@game.finished = true
+				@game.started = false
+				@game.save
+				user_leave(current_user)
+			end
 		end
-		@game.calc_stakes
-		@game.get_stakes_for_outcome
-		@game.calc_winner_ratings
-		@game.calc_loser_ratings
-		@game.finished = true
-		@game.started = false
-		@game.save
-		user_leave(current_user)
 	end
 
 	def vote_dire
-		current_user.has_vote = false
-		current_user.save
+		if logged_in
+			current_user.has_vote = false
+			current_user.save
 
-		@game = Game.find_by(id: params[:gameid])
-		@game.dire_votes += 1
-		if @game.dire_votes >= 6
-			@game.winner = "dire"
-			@game.loser = "radiant"
+			@game = Game.find_by(id: params[:gameid])
+			@game.dire_votes += 1
+			if @game.dire_votes >= 6
+				@game.winner = "dire"
+				@game.loser = "radiant"
+			
+				@game.calc_stakes
+				@game.get_stakes_for_outcome
+				@game.calc_winner_ratings
+				@game.calc_loser_ratings
+				@game.finished = true
+				@game.started = false
+				@game.save
+				user_leave(current_user)
+			end
 		end
-		@game.calc_stakes
-		@game.get_stakes_for_outcome
-		@game.calc_winner_ratings
-		@game.calc_loser_ratings
-		@game.finished = true
-		@game.started = false
-		@game.save
-		user_leave(current_user)
 	end
 
 	private
