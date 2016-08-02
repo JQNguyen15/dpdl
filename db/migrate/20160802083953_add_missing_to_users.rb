b4f2b4c204c7af80b9a7e419f217f79660fff74e
James <jamesnguyen23@gmail.com>
2016-08-02 14:01:33 -0400

    added autorefeshing and SSL

--- db/migrate/20160802083953_add_missing_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddMissingToUsers < ActiveRecord::Migration[5.0]
-  def change
-    add_column :users, :skill, :float, default: 25.0
-    add_column :users, :doubt, :float, default: 8.333
-  end
-end
