97cea2e3359b0edca6b517b45778178e65005ea6
James <jamesnguyen23@gmail.com>
2016-08-03 17:43:52 -0400

    fixed game create action cable

--- app/assets/javascripts/channels/games.js
+++ app/assets/javascripts/channels/games.js
@@ -1,13 +1,12 @@
 App.messages = App.cable.subscriptions.create('GamesChannel', {  
   received: function(data) {
     $("#open-games").removeClass('hidden')
-    //$('#comments ul.comments').append("<%= escape_javascript render(:partial => 'comments/single', :locals => { :c => @comment }) %>");
     return $('#open-games').append(this.renderMessage(data));
   },
 
   renderMessage: function(data) {
   	var content = "<ul><b>Game ID</b>:" + data.gameid + " <b>Host</b>: " + data.host + "<br><u>Players</u> " + data.numPlayers + "<br>"
-  	+ "<li>" + data.host + " " + data.hostmmr + "</ul><br>";
+  	+ "<li>" + data.host + " " + data.hostmmr + "<br><a class='btn btn-success' href='/join_game?gameid=" + data.gameid + "'>Join Game</a></ul>";
     return content;
   }
 });
