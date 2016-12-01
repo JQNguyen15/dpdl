2f2aaa1ce9caf156c309b37fab00302d5a71e5bf
James Nguyen <james.nguyen@pillrcompany.com>
2016-12-01 01:19:55 -0500

    Migration fixs for smoother heroku db changes

--- db/migrate/20161125141809_add_skill_to_users.rb
+++ db/migrate/20161125141809_add_skill_to_users.rb
@@ -1,5 +1,8 @@
 class AddSkillToUsers < ActiveRecord::Migration[5.0]
   def change
+    remove_column :users, :skill, :float
+  end
+  def change
     add_column :users, :skill, :float, :default => 25.0
   end
 end
--- db/migrate/20161125141928_add_doubt_to_users.rb
+++ db/migrate/20161125141928_add_doubt_to_users.rb
@@ -1,5 +1,8 @@
 class AddDoubtToUsers < ActiveRecord::Migration[5.0]
   def change
+    remove_column :users, :doubt, :float
+  end
+  def change
     add_column :users, :doubt, :float, :default => 8.333333333333334
   end
 end
